24ba7b49d0f8ca67bb783c42abb17a5aebbb0c5b
Damianus Clairvoyance <13520035@std.stei.itb.ac.id>
2023-05-23 11:45:14 +0700

    feat: page structure and styling ok

---
+++ quasar-project/src/components/TaskItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <p>{{ task.name }}</p>
+        <p>{{ task.dueDate }} @{{ task.dueTime }} at {{ index }}</p>
+    </li>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'TaskItem',
+        props: ['task', 'index']
+    })
+</script>
+
+<style>
+</style>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,104 @@
+<template>
+  <q-page class="page-color q-px-lg q-py-sm">
+    <ul class="item-color shadow-6">
+      <task
+        v-for="(task) in tasks"
+        :key="task.id"
+        :task="task"></task>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: "Go to shops",
+          dueDate: "2023/05/21",
+          dueTime: "18:30"
+        },
+        {
+          id: 2,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 3,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 4,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 5,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 6,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 7,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 8,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 9,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 10,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 11,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        },
+        {
+          id: 12,
+          name: "Get bananas",
+          dueDate: "2023/05/22",
+          dueTime: "16:00"
+        }
+      ]
+    }
+  },
+  components: {
+    'task': require('components/TaskItem.vue').default
+  }
+})
+</script>
+
+<style>
+    .item-color {
+        background-color: white;
+    }
+</style>
